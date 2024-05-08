81e8c1ffca49a2e8d4f6b968d755f81743d7017e
arshan19 <aliarshan1934@gmail.com>
2024-05-08 14:09:35 +0530

    add some explanation and comments in three and four files in 05_iterations folder

--- 05_iterations/03_three.js
+++ 05_iterations/03_three.js
@@ -12,24 +12,24 @@ for (const num of arr) {
 const greetings = "Hello world!"
 for (const greet of greetings) {
     if (greet == " "){
-        continue  //use to eleminate extra-space , if we want to exit our loop after extra-space then we can use break;
+        continue;  //use to eleminate extra-space , if we want to exit our loop after extra-space then we can use break;
     }
     //console.log(`Each char is ${greet}`)
 }
 
 // Maps
 
-const map = new Map()
-map.set('IN', "India")
+const map = new Map() //map is like object as mostly everythink in JS is Object , but maps have unique keys
+map.set('IN', "india")
 map.set('USA', "United States of America")
 map.set('Fr', "France")
-map.set('IN', "India")
+map.set('IN', "India") //unique values, doesn't became duplicate 
 
 
 // console.log(map);
 
-for (const [key, value] of map) {
-    // console.log(key, ':-', value);
+for (const [key,value] of map) {     // we can iterate in maps using [] square bracket and then destructure the keys,values of maps accordingly
+     console.log(key,"->",value);
 }
 
 const myObject = {
@@ -37,7 +37,10 @@ const myObject = {
     game2: 'Spiderman'
 }
 
-// for (const [key, value] of myObject) {
-//     console.log(key, ':-', value);
-    
-// }
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value); //shows an error , which solve in next file using FOR IN loop
+}
+
+/**here object are not iterable through forof loop, but maps are iterable through forof loop
+ * so for using loops interation on object we use ForIn loops in next file 04_four.js
+ */
--- 05_iterations/04_four.js
+++ 05_iterations/04_four.js
@@ -1,3 +1,4 @@
+/*using For In loop on object to iterate on object */
 const myObject = {
     js: 'javascript',
     cpp: 'C++',
@@ -5,16 +6,22 @@ const myObject = {
     swift: "swift by apple"
 }
 
-for (const key in myObject) {
-    //console.log(`${key} shortcut is for ${myObject[key]}`);
+for (const value in myObject) {
+    
+    //console.log(`${key} shortcut is for ${myObject[key]}`); //here first we take just keys, and after that by using object myObject and
+                                                        // then take the value of keys which we inserted in keys like this myObject[keys] to takeout the value of keys.
 }
+
+/*now we check can we insert For in loop on array  */
 
 const programming = ["js", "rb", "py", "java", "cpp"]
 
 for (const key in programming) {
-    //console.log(programming[key]);
+    //console.log(key);  //result : 0 1 2 3 4 => so it will iterate in "FOR IN" loop and gives keys as result which is 01234 in array as predefined,whereas when we use "FOROF" loop we get values directly [this is one major difference in both of them]
+    //console.log(programming[key]); //getting values from keys
 }
 
+//now check for maps , does FOR IN loop work in maps ?
 // const map = new Map()
 // map.set('IN', "India")
 // map.set('USA', "United States of America")
@@ -24,3 +31,5 @@ for (const key in programming) {
 // for (const key in map) {
 //     console.log(key);
 // }
+
+//so the result is unique : we don't get anything here , we got no error and also no result also , because we can't iterate in maps , will discuss futher this in future.
