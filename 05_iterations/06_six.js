90e36415e55afaf83160c324553efb8f8de6337f
arshan19 <aliarshan1934@gmail.com>
2024-05-09 00:04:47 +0530

    explain what is filter and how we use it irrespective to forEach Loop

--- 05_iterations/06_six.js
+++ 05_iterations/06_six.js
@@ -1,28 +1,37 @@
 // const coding = ["js", "ruby", "java", "python", "cpp"]
 
+/*If we want to store this callback function of FOREACH loop into a variable, do this return me any value ?
+ so , forEach loop does not return any value
+ so what that purpose we have other method as well like we use filter below.*/
 
 // const values = coding.forEach( (item) => {
 //     //console.log(item);
 //     return item
 // } )
 
-// console.log(values);
+// console.log(values); 
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNums.filter( (num) => {
-//     return num > 4
-// } )
+/* filter opearion: it return the value which return true to the condition*/
 
-// const newNums = []
+ const newNums = myNums.filter( (num) => {
+     return num > 4  /**** if we complete our arrow function in one line,we doesn't required return keyword but if we use {} braces(starting of our scope) and implement our function in next line ,then we required return keyword.***/
+ } )
+
+/* how to use ForEach loop to return values in new variable */
+
+// const newNums2 = []
 
 // myNums.forEach( (num) => {
 //     if (num > 4) {
-//         newNums.push(num)
+//         newNums2.push(num)
 //     }
 // } )
 
-//console.log(newNums);
+// console.log(newNums2);
+
+
 
 
 const books = [
@@ -36,10 +45,14 @@ const books = [
     { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
   ];
-
-  let userBooks = books.filter( (bk) => bk.genre === 'History')
+  let userBooks = books.filter( (bk) => bk.genre === 'History') 
+  //implement filter books array where we have different object and each object contain some value(item) here which is bk here and then we extract bk property or method we have in our object like title,genre,published,edition here and according to dot method we can search it out and implement it
 
   userBooks = books.filter( (bk) => { 
     return bk.publish >= 1995 && bk.genre === "History"
-})
+  })
   console.log(userBooks);
+
+  //for my learning prespective
+  const getBooks = books.filter( (bk) =>  bk.genre === 'Science' || bk.publish > 1990 )
+  console.log(getBooks);
